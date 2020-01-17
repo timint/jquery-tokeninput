3f7aadf902080dd3910f0582d63f8bfd157440c8
timint <info@tim-international.net>
2020-01-17 05:06:28 +0100

    + Add som grunt modules (LESS compiler and watcher)

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,36 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    replace: {
+      index: {
+        src: ['src/*'],
+        overwrite: true,
+        replacements: [
+          {
+            from: /Version ([0-9\.]+)/,
+            to: 'Version <%= pkg.version %>'
+          }
+        ]
+      },
+    },
+
+    less: {
+      tokeninput_minified: {
+        options: {
+          compress: true,
+          sourceMap: true,
+          sourceMapBasepath: 'src/',
+          sourceMapRootpath: './',
+          sourceMapURL: function(path) { return path.replace(/.*\//, '') + '.map'; },
+          relativeUrls: true
+        },
+        files: {
+          'build/jquery.tokeninput.min.css'       : 'src/jquery.tokeninput.less',
+        }
+      },
+    },
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
@@ -12,13 +42,24 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+
+    watch: {
+      less: {
+        files: [
+          'src/jquery.tokeninput.less',
+        ],
+        tasks: ['less']
+      },
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-text-replace');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['replace', 'less', 'uglify']);
 
 };
